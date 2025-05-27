f858812082240586afb87f326e71302b75874e8f
Josesx506 <45031412+Josesx506@users.noreply.github.com>
2025-05-27 15:56:31 -0600

    fix broken build following svgr webpack uninstall

--- next.config.mjs
+++ next.config.mjs
@@ -16,13 +16,6 @@ const nextConfig = {
             },
         ],
     },
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ['@svgr/webpack'],
-        });
-        return config;
-    },
 };
 
 export default nextConfig;
